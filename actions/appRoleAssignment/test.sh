d45be38349cbb26a344e61d7323924408d68e26a
Thomas Dupoiron <tdupoiron@254gj2.onmicrosoft.com>
2023-06-28 15:31:50 +0000

    finish action.yml

--- actions/appRoleAssignment/test.sh
+++ actions/appRoleAssignment/test.sh
@@ -1,6 +1,6 @@
 export INPUT_AZURE_APP_NAME="GitHub Enterprise Managed User"
 export INPUT_AZURE_RESOURCE_TYPE="group"
 export INPUT_AZURE_RESOURCE_NAME="Sales and Marketing"
-export INPUT_AZURE_ROLE_NAME="Enterprise Owner"
+export INPUT_AZURE_ROLE_NAME="User"
 
 node index.js
