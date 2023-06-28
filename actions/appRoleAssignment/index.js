e5e67d26a6556c0e3e3f4c15251ce8ef99883e0f
Thomas Dupoiron <tdupoiron@254gj2.onmicrosoft.com>
2023-06-28 15:27:59 +0000

    update variable names

--- actions/appRoleAssignment/index.js
+++ actions/appRoleAssignment/index.js
@@ -6,26 +6,28 @@ const AUTHORITY_HOST_URL = 'https://login.microsoftonline.com';
 const GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0';
 
 // Read inputs from workflow file
-const CLIENT_ID = core.getInput('CLIENT_ID');
-const CLIENT_SECRET = core.getInput('CLIENT_SECRET');
-const TENANT_ID = core.getInput('TENANT_ID');
+const AZURE_CLIENT_ID = core.getInput('AZURE_CLIENT_ID');
+const AZURE_CLIENT_SECRET = core.getInput('AZURE_CLIENT_SECRET');
+const AZURE_TENANT_ID = core.getInput('AZURE_TENANT_ID');
 
-const GITHUB_APP_NAME = core.getInput('GITHUB_APP_NAME');
-const GITHUB_RESOURCE_TYPE = core.getInput('GITHUB_RESOURCE_TYPE');
-const GITHUB_RESOURCE_NAME = core.getInput('GITHUB_RESOURCE_NAME');
-const GITHUB_ROLE_NAME = core.getInput('GITHUB_ROLE_NAME');
+const AZURE_APP_NAME = core.getInput('AZURE_APP_NAME');
+const AZURE_RESOURCE_TYPE = core.getInput('AZURE_RESOURCE_TYPE');
+const AZURE_RESOURCE_NAME = core.getInput('AZURE_RESOURCE_NAME');
+const AZURE_ROLE_NAME = core.getInput('AZURE_ROLE_NAME');
 
 // Function to get access token
 async function getAccessToken() {
 
-    const data = `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&scope=${SCOPE}`;
+    const data = `grant_type=client_credentials&client_id=${AZURE_CLIENT_ID}&client_secret=${AZURE_CLIENT_SECRET}&scope=${SCOPE}`;
+    core.debug(`data: ${data}`);
+
     const config = {
         headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
         }
     };
 
-    return axios.post(`${AUTHORITY_HOST_URL}/${TENANT_ID}/oauth2/v2.0/token`, data, config).then((response) => {
+    return axios.post(`${AUTHORITY_HOST_URL}/${AZURE_TENANT_ID}/oauth2/v2.0/token`, data, config).then((response) => {
         return response.data.access_token;
     }).catch((error) => {
         core.setFailed(error);
@@ -46,7 +48,7 @@ async function invokeGraphGet(token, url) {
         return response.data;
     }).catch((error) => {
         core.setFailed(error);
-        throw error;
+        throw error.response.data.error;
     });
 
 }
@@ -82,7 +84,7 @@ async function assignRole(token, appId, roleId, principalId) {
         return response.data;
     }).catch((error) => {
         core.setFailed(error);
-        throw error;
+        throw error.response.data.error;
     });
 
 }
@@ -90,22 +92,22 @@ async function assignRole(token, appId, roleId, principalId) {
 
 async function main() {
 
-    core.info(`Assigning role ${GITHUB_ROLE_NAME} to ${GITHUB_RESOURCE_TYPE} ${GITHUB_RESOURCE_NAME} for app ${GITHUB_APP_NAME}`)
+    core.info(`Assigning role ${AZURE_ROLE_NAME} to ${AZURE_RESOURCE_TYPE} ${AZURE_RESOURCE_NAME} for app ${AZURE_APP_NAME}`)
 
     var token = await getAccessToken();
     core.debug(`token: ${token}`);
 
-    var app = await getApplicationFromName(token, GITHUB_APP_NAME);
+    var app = await getApplicationFromName(token, AZURE_APP_NAME);
     
-    if (GITHUB_RESOURCE_TYPE == 'group') {
-        var group = await getGroupFromName(token, GITHUB_RESOURCE_NAME);
+    if (AZURE_RESOURCE_TYPE == 'group') {
+        var group = await getGroupFromName(token, AZURE_RESOURCE_NAME);
     } else {
-        var user = await getUserFromName(token, GITHUB_RESOURCE_NAME);
+        var user = await getUserFromName(token, AZURE_RESOURCE_NAME);
     }
 
     var appId = app.value[0].id;
-    var roleId = app.value[0].appRoles.filter(role => role.displayName == GITHUB_ROLE_NAME)[0].id;
-    var principalId = GITHUB_RESOURCE_TYPE == 'group' ? group.value[0].id : user.value[0].id;
+    var roleId = app.value[0].appRoles.filter(role => role.displayName == AZURE_ROLE_NAME)[0].id;
+    var principalId = AZURE_RESOURCE_TYPE == 'group' ? group.value[0].id : user.value[0].id;
 
     if (appId == null || roleId == null || principalId == null) {
         core.setFailed('Could not find app, role or principal');
